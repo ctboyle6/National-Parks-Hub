3b38a234d08d464ce4b97bcd5c8805c00a3c01d6
Chris Boyle <cthboyle6@gmail.com>
2021-06-24 15:04:01 -0400

    API controller actions added for parks and reviews

--- app/controllers/api/v1/parks_controller.rb
+++ app/controllers/api/v1/parks_controller.rb
@@ -1,6 +1,57 @@
 module API
   module V1
     class ParksController < ApplicationController
+      def index
+        parks = Park.all
+
+        render json: ParkSerializer.new(parks, options).serialized_json
+      end
+
+      def show
+        park = Park.find_by(park_code: params[:park_code])
+
+        render json: ParkSerializer.new(park, options).serialized_json
+      end
+
+      def create
+        park = Park.new(park_params)
+
+        if park.save
+          render json: ParkSerializer.new(park).serialized_json
+        else
+          render json: { error: park.errors.messages }, status: 422
+        end
+      end
+
+      def update
+        park = Park.find_by(park_code: params[:park_code])
+
+        if park.update(park_params)
+          render json: ParkSerializer.new(park, options).serialized_json
+        else
+          render json: { error: park.errors.messages }, status: 422
+        end
+      end
+
+      def destroy
+        park = Park.find_by(park_code: params[:park_code])
+
+        if park.destroy
+          head :no_content
+        else
+          render json: { error: park.errors.messages }, status: 422
+        end
+      end
+
+      private
+
+      def park_params
+        params.require(:park).permit(:name, :latitude, :longitude, :park_code)
+      end
+
+      def options
+        @options ||= { include: %i[reviews] }
+      end
     end
   end
 end
--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,6 +1,31 @@
 module API
   module V1
     class ReviewsController < ApplicationController
+      def create
+        review = Review.new(review_params)
+
+        if review.save
+          render json: ReviewSerializer.new(review).serialized_json
+        else
+          render json: { error: review.errors.messages }, status: 422
+        end
+      end
+
+      def destroy
+        review = Review.find_by(params[:id])
+
+        if review.destroy
+          head :no_content
+        else
+          render json: { error: review.errors.messages }, status: 422
+        end
+      end
+
+      private
+
+      def review_params
+        params.require(:review).permit(:title, :description, :rating, :park_id)
+      end
     end
   end
 end
