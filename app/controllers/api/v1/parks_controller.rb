b2cdac0ecbe1b8326b9587bf7b468f4d57b4b390
Chris Boyle <cthboyle6@gmail.com>
2021-06-24 14:56:19 -0400

    init api controllers

---
+++ app/controllers/api/v1/parks_controller.rb
@@ -0,0 +1,6 @@
+module API
+  module V1
+    class ParksController < ApplicationController
+    end
+  end
+end
---
+++ app/controllers/api/v1/reviews_controller.rb
@@ -0,0 +1,6 @@
+module API
+  module V1
+    class ReviewsController < ApplicationController
+    end
+  end
+end
