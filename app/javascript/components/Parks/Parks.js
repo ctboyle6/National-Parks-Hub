bc5f843da24784e522173bd08beb50bcaf5433c2
Chris Boyle <cthboyle6@gmail.com>
2021-06-24 15:47:32 -0400

    effect pull parks into component state, test list to output parks

--- app/javascript/components/Parks/Parks.js
+++ app/javascript/components/Parks/Parks.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Parks = () => {
+  const [ parks, setParks ] = useState([])
+
+  useEffect(() => {
+    // Get all parks from our API
+    // update parks in our state array
+
+    axios.get('/api/v1/parks.json')
+      .then((res) => {
+        setParks(res.data.data)
+      })
+      .catch((res) => console.log(res))
+  }, [parks.length])
+
+  const list = parks.map((item) => {
+    return (
+      <li key={item.attributes.name}>{item.attributes.name}</li>
+    )
+  })
+
   return (
-    <div>This is the Parks#index view for our App</div>
+    <div>
+      <div>This is the Parks#index view for our App</div>
+      <ul>{list}</ul>
+    </div>
   )
 }
 
