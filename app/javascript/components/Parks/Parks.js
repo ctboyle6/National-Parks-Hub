45d7c573384e9d431ea843670d57840a2a24827c
Chris Boyle <cthboyle6@gmail.com>
2021-06-25 12:26:59 -0400

    styled-components added to home, grid, header, and subheader

--- app/javascript/components/Parks/Parks.js
+++ app/javascript/components/Parks/Parks.js
@@ -1,6 +1,34 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Park from './Park'
+import styled from 'styled-components'
+
+const Dashboard = styled.div`
+  text-align: center;
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+`
+
+const Header = styled.div`
+  padding: 100px 100px 10px 100px;
+
+  h1 {
+    font-size: 42px;
+  }
+`
+
+const Subheader = styled.div`
+  font-weight: 300;
+  font-size: 26px;
+`
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+  width: 100%;
+  padding: 20px;
+`
 
 const Parks = () => {
   const [ parks, setParks ] = useState([])
@@ -26,15 +54,15 @@ const Parks = () => {
   })
 
   return (
-    <div className="home">
-      <div className="header">
+    <Dashboard>
+      <Header>
         <h1>National Parks Hub</h1>
-      </div>
-      <div className="subheader">Get out and hike!</div>
-      <div className="grid">
+      </Header>
+      <Subheader>Get out and hike!</Subheader>
+      <Grid>
         <ul>{grid}</ul>
-      </div>
-    </div>
+      </Grid>
+    </Dashboard>
   )
 }
 
