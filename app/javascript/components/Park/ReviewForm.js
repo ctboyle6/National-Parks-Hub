077ef0b3f1f6f4b39b5c1d909ac40fb9f100cc00
Chris Boyle <cthboyle6@gmail.com>
2021-06-28 10:43:53 -0400

    star rating system style with unselected, hover, and selected

--- app/javascript/components/Park/ReviewForm.js
+++ app/javascript/components/Park/ReviewForm.js
@@ -1,4 +1,51 @@
 import React, { Fragment } from 'react'
+import styled from 'styled-components'
+import Gray from './Stars/Gray'
+import Hover from './Stars/Hover'
+import Selected from './Stars/Selected'
+
+const RatingContainer = styled.div`
+  text-align: center;
+  border-radius: 4px;
+  font-size: 18px;
+  padding: 40px 0 10px 0;
+  border: 1px solid #e6e6e6;
+  background: #fff;
+`
+
+const RatingBox = styled.div`
+  background: #fff;
+  display: flex;
+  justify-content: center;
+  flex-direction: row-reverse;
+  position: relative;
+
+  input {
+    display: none;
+  }
+
+  label {
+    cursor: pointer;
+    width: 40px;
+    height: 40px;
+    background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='126.729' height='126.73'%3e%3cpath fill='%23e3e3e3' d='M121.215 44.212l-34.899-3.3c-2.2-.2-4.101-1.6-5-3.7l-12.5-30.3c-2-5-9.101-5-11.101 0l-12.4 30.3c-.8 2.1-2.8 3.5-5 3.7l-34.9 3.3c-5.2.5-7.3 7-3.4 10.5l26.3 23.1c1.7 1.5 2.4 3.7 1.9 5.9l-7.9 32.399c-1.2 5.101 4.3 9.3 8.9 6.601l29.1-17.101c1.9-1.1 4.2-1.1 6.1 0l29.101 17.101c4.6 2.699 10.1-1.4 8.899-6.601l-7.8-32.399c-.5-2.2.2-4.4 1.9-5.9l26.3-23.1c3.8-3.5 1.6-10-3.6-10.5z'/%3e%3c/svg%3e");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 70%;
+  }
+
+  input:checked ~ label,
+  input:checked ~ label ~ label {
+    background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='126.729' height='126.73'%3e%3cpath fill='%23fcd93a' d='M121.215 44.212l-34.899-3.3c-2.2-.2-4.101-1.6-5-3.7l-12.5-30.3c-2-5-9.101-5-11.101 0l-12.4 30.3c-.8 2.1-2.8 3.5-5 3.7l-34.9 3.3c-5.2.5-7.3 7-3.4 10.5l26.3 23.1c1.7 1.5 2.4 3.7 1.9 5.9l-7.9 32.399c-1.2 5.101 4.3 9.3 8.9 6.601l29.1-17.101c1.9-1.1 4.2-1.1 6.1 0l29.101 17.101c4.6 2.699 10.1-1.4 8.899-6.601l-7.8-32.399c-.5-2.2.2-4.4 1.9-5.9l26.3-23.1c3.8-3.5 1.6-10-3.6-10.5z'/%3e%3c/svg%3e");
+  }
+
+  input:not(:checked) ~ label:hover,
+  input:not(:checked) ~ label:hover ~ label {
+    background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='126.729' height='126.73'%3e%3cpath fill='%23d8b11e' d='M121.215 44.212l-34.899-3.3c-2.2-.2-4.101-1.6-5-3.7l-12.5-30.3c-2-5-9.101-5-11.101 0l-12.4 30.3c-.8 2.1-2.8 3.5-5 3.7l-34.9 3.3c-5.2.5-7.3 7-3.4 10.5l26.3 23.1c1.7 1.5 2.4 3.7 1.9 5.9l-7.9 32.399c-1.2 5.101 4.3 9.3 8.9 6.601l29.1-17.101c1.9-1.1 4.2-1.1 6.1 0l29.101 17.101c4.6 2.699 10.1-1.4 8.899-6.601l-7.8-32.399c-.5-2.2.2-4.4 1.9-5.9l26.3-23.1c3.8-3.5 1.6-10-3.6-10.5z'/%3e%3c/svg%3e");
+  }
+`
+
+const RatingTitle = styled.div``
 
 const ReviewForm = (props) => {
   const ratingOptions = [5, 4, 3, 2, 1].map( (rating, index) => {
@@ -21,10 +68,12 @@ const ReviewForm = (props) => {
           <input onChange={props.handleChange} value={props.review.description} type="text" name="description" placeholder="Review Description"/>
         </div>
         <div className="field">
-          <div className="rating-container">
+          <RatingContainer>
             <div className="rating-title-text">Rate This Park</div>
-            {ratingOptions}
-          </div>
+            <RatingBox>
+              {ratingOptions}
+            </RatingBox>
+          </RatingContainer>
         </div>
         <button type="submit">Submit Your Review</button>
       </form>
