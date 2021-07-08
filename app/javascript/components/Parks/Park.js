81acb0eabe5e1ee6c025d76cf6da76dbbd9d5c14
Chris Boyle <cthboyle6@gmail.com>
2021-07-08 10:28:51 -0400

    modal styling

--- app/javascript/components/Parks/Park.js
+++ app/javascript/components/Parks/Park.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Link } from 'react-router-dom'
 import styled from 'styled-components'
 import Rating from '../Rating/Rating'
