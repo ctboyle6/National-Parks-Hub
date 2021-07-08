d86f7f6c941a91e66ca41dc00f7f69c2b6c8f708
Chris Boyle <cthboyle6@gmail.com>
2021-07-08 11:47:14 -0400

    fix modal to full window

--- app/javascript/components/Modal.js
+++ app/javascript/components/Modal.js
@@ -3,10 +3,12 @@ import styled from 'styled-components'
 import { MdClose } from 'react-icons/md'
 
 const Background = styled.div`
-    width: 100%;
-    height: 100%;
+    width: 100vw;
+    height: 100vh;
     background: rgba(0, 0, 0, 0.8);
     position: fixed;
+    top: 0;
+    left: 0;
     display: flex;
     justify-content: center;
     align-items: center;
