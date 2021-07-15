3e0d542b42db501c3f68216c174ba9af0ad429c4
Chris Boyle <cthboyle6@gmail.com>
2021-07-15 14:18:14 -0400

    new star elements and styling

--- app/javascript/components/Rating/Rating.js
+++ app/javascript/components/Rating/Rating.js
@@ -5,9 +5,26 @@ const Rating = (props) => {
   const score = (props.rating/5) * 100
 
   return (
-    <span className="star-wrapper">
-      <span className="stars" style={{width: score + "%"}}></span>
-    </span>
+    // <span className="star-wrapper">
+    //   <span className="stars" style={{width: score + "%"}}></span>
+    // </span>
+
+    <div className="rating">
+      <div className="rating-upper" style={{width: score + "%"}}>
+          <span>★</span>
+          <span>★</span>
+          <span>★</span>
+          <span>★</span>
+          <span>★</span>
+      </div>
+      <div className="rating-lower">
+          <span>★</span>
+          <span>★</span>
+          <span>★</span>
+          <span>★</span>
+          <span>★</span>
+      </div>
+    </div>
   )
 }
 
